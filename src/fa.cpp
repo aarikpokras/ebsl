f0088ba3688da5036a62098cc6806412109bd7d7
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-11-16 21:51:58 -0500

    Add logic for max

---
+++ src/fa.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "fa.hpp"
+using namespace std;
+
+size_t find_a(string str) {
+    size_t amt = 0;
+    while (str.length() != 0) {
+        size_t index = str.find(",");
+        amt++;
+        str.erase(0, index+1);
+    }
+    return amt;
+}
