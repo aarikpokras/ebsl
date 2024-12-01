93b9dd1aa3c33755f4f1ff5205271e885cfc99e2
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2024-11-30 23:33:24 -0500

    Fix more values showing up for mac

--- src/aml.cpp
+++ src/aml.cpp
@@ -16,18 +16,20 @@ vector<string> gval_aml(string key, string file, size_t amount) {
         return ret_vec;
     }
     while(getline(fileTP, ln)) {  // On condition that file is opened, so it is fine to use ret_vec
-        if (amount == 0) {
-            amount = find_a(ln);
-        }
         if (regex_search(ln, key_r)) {
+            if (amount == 0) {
+                amount = find_a(ln);
+            }
             string ret_vp_ = regex_replace(ln, key_r, "");
             for (size_t i = 0; i < amount; i++) {
                 size_t ret_find = ret_vp_.find(",");
-                ret_vec.push_back(ret_vp_.substr(0, ret_find));
-                ret_vp_.erase(0, ret_find+1);
+                if (ret_find <= ret_vp_.size()) {
+                    ret_vec.push_back(ret_vp_.substr(0, ret_find));
+                    ret_vp_.erase(0, ret_find+1);                    
+                } else {
+                    ret_vec.push_back(ret_vp_);
+                }
             }
-        } else if (regex_search(ln, com)) {
-            // Do nothing
         }
     }
     fileTP.close();
