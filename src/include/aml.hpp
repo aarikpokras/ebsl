0a0abbf1e148daf2478e655369a0644e322e242e
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2025-12-29 01:58:09 -0500

    Add indelim parameter to gval_aml function declaration

--- src/include/aml.hpp
+++ src/include/aml.hpp
@@ -2,5 +2,5 @@
 #include <vector>
 #ifndef AML_H
 #define AML_H
-std::vector<std::string> gval_aml(std::string key, std::string file, size_t amount);
+std::vector<std::string> gval_aml(std::string key, std::string file, size_t amount, std::string indelim);
 #endif
